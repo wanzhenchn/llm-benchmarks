2fad69d393f50711854d560328b83f9e2b888e0c
wanzhenchn <wanzhenchn@gmail.com>
2024-08-06 18:31:43 +0800

    update serve_openai_api.sh for vllm

--- scripts/vllm/serve_openai_api.sh
+++ scripts/vllm/serve_openai_api.sh
@@ -13,43 +13,31 @@ set -euxo pipefail
 
 host_name=0.0.0.0
 
-if [ $# = 4 ] || [ $# = 5 ]; then
+if [ $# = 4 ]; then
   mode=$1
   model_path=$2
+  port=$3
   device_id=${!#}
 
-  if [ $# = 5 ]; then
-    module_path=$3
-    port=$4
-  else
-    port=$3
-  fi
   gpu_num=$(echo "$device_id" |grep -o "[0-9]" |grep -c "")
   export CUDA_VISIBLE_DEVICES=$device_id
 
   extra_args=""
 
-  if [ $mode = fp16 ]; then
-    extra_args+="--dtype auto " # “auto” will use FP16 precision for FP32 and FP16 models, and BF16 precision for BF16 models
-    extra_args+="--enable-prefix-caching "
+  if [ $mode = fp16 ] || [ $mode = w4a16 ] || [ $mode = kv-fp8 ] || [ $mode = fp8-kv-fp16 ] || [ $mode = fp8-kv-fp8 ]; then
+    if [ $mode = w4a16 ]; then
+      extra_args+="--dtype half "
+    fi
 
-  elif [ $mode = w4a16 ]; then
-    extra_args+="--dtype half "
-    extra_args+="--enable-prefix-caching "
-    extra_args+="--quantization awq"
+    if [ $mode = fp8-kv-fp8 ] || [ $mode = kv-fp8 ]; then
+      extra_args+="--kv-cache-dtype fp8 "
+    else
+      extra_args+="--enable-prefix-caching "
+    fi
 
-  elif [ $mode = fp8 ]; then
-    extra_args+="--dtype auto "
-    extra_args+="--quantization fp8 "
-    extra_args+="--kv-cache-dtype fp8 " # auto, fp8, fp8_e5m2, fp8_e4m3
-    extra_args+="--quantization-param-path ${module_path} "
-
-  elif [ $mode = lora ]; then
-    extra_args+="--dtype auto "
-    extra_args+="--enable-prefix-caching "
-    extra_args+="--enable-lora "
-    extra_args+="--max-lora-rank 32 "
-    extra_args+="--lora-modules ${module_path}" #{name}={path} {name}={path}
+  else
+    echo "mode only support fp16, w4a16, kv-fp8, fp8-kv-fp16 and fp8-kv-fp8"
+    exit
   fi
 
   python3 -m vllm.entrypoints.openai.api_server \
@@ -58,18 +46,15 @@ if [ $# = 4 ] || [ $# = 5 ]; then
     --model ${model_path} \
     -tp ${gpu_num} \
     --max-model-len 4096 \
-    --max-num-seqs 256 `# How many requests can be batched into a single model run` \
+    --max-num-seqs 512 `# How many requests can be batched into a single model run` \
     --gpu-memory-utilization 0.9 \
     --swap-space 16 `# CPU swap space size (GiB) per GPU.` \
+    --trust-remote-code \
     --disable-log-stats \
     --disable-log-requests \
     ${extra_args}
-#    --uvicorn-log-level info `# ['debug', 'info', 'warning', 'error', 'critical', 'trace']`
-#    --quantization ``
 #    --enforce-eager `# By default it's not eager mode, If it's not set, it uses cuda_graph`
 else
-  echo "Usage1: $0 mode(fp16, w4a16 or kv8) model_path port device_id(0 or 0,1 or 1,3)"
-  echo "Usage2: $0 mode(fp8) model_path kv_cache_scales_path port device_id(0 or 0,1 or 1,3)"
-  echo "Usage3: $0 mode(lora) model_path lora_modules_path port device_id(0 or 0,1 or 1,3)"
+  echo "Usage1: $0 mode(fp16, w4a16 or fp8-kv-fp8, fp8-kv-fp16) model_path port device_id(0 or 0,1 or 1,3)"
   exit
 fi
