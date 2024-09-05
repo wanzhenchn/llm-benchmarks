7c7723e919e7c65d4374e428b88470721da83fd5
wanzhenchn <wanzhenchn@gmail.com>
2024-09-05 15:04:58 +0800

    update Dockerfile for trt-llm

--- scripts/vllm/build_docker.sh
+++ scripts/vllm/build_docker.sh
@@ -12,7 +12,7 @@ fi
 cd vllm
 
 VERSION=$(grep '^__version__' ./vllm/version.py | grep -o '=.*' | tr -d '= "')
-cuda_arch_list="7.0 8.0 8.9 9.0"
+cuda_arch_list="7.0 8.0 9.0"
 arch=arch_$(echo $cuda_arch_list | tr -d -c 0-9)
 
 IMAGE_TAG=registry.cn-beijing.aliyuncs.com/devel-img/vllm:${VERSION}-${arch}
